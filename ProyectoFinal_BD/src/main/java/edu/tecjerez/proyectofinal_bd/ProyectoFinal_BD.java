79872c0054227f812574a67d5d0d1188681a3910
PerseoN15 <159813180+PerseoN15@users.noreply.github.com>
2024-11-19 17:13:47 -0600

    Creacion del proyecto
    El proyecto fue creado

---
+++ ProyectoFinal_BD/src/main/java/edu/tecjerez/proyectofinal_bd/ProyectoFinal_BD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.tecjerez.proyectofinal_bd;
+
+/**
+ *
+ * @author anemn
+ */
+public class ProyectoFinal_BD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
