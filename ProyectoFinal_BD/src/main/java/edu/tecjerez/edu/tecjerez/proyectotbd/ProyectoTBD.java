c7446dff56a2492f4e1c6bebe1220d7e03c7fc69
PerseoN15 <159813180+PerseoN15@users.noreply.github.com>
2024-11-30 15:11:52 -0600

    Se agreo una clase

---
+++ ProyectoFinal_BD/src/main/java/edu/tecjerez/edu/tecjerez/proyectotbd/ProyectoTBD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.tecjerez.proyectotbd;
+
+/**
+ *
+ * @author anemn
+ */
+public class ProyectoTBD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
